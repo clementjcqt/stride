1899a2fc3ef848e8ddd34890204a5adf9c8c12c6
clement jacquet <clement.jacquet.16@gmail.com>
2025-08-19 16:49:57 +0200

    started index and events cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
 p "starting seeding..."
 unless User.find_by(email: "toto@toto.fr")
   User.create(email: "toto@toto.fr", password: "123456")
+end
+
+10.times do
+  event = Event.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    location: Faker::Locations::Australia.location,
+    date: Faker::Date.forward(days: 365),
+    event_type: ['trail', 'marathon', 'semi-marathon'].sample,
+    distance_km: rand(100),
+    description: Faker::Lorem.sentence(word_count: 15),
+    difficulty: ['easy', 'intermediate', 'hard'].sample,
+    denivele: rand(1000)
+  )
+  p event
 end
 
 p "user created: #{User.last.email}"
