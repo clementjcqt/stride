fb5fb0a7fec795de4474177e6eeb9230e4148853
clement jacquet <clement.jacquet.16@gmail.com>
2025-08-19 15:50:46 +0200

    fixed table and started show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_08_18_154401) do
+ActiveRecord::Schema[7.1].define(version: 2026_08_18_154402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,9 +65,10 @@ ActiveRecord::Schema[7.1].define(version: 2026_08_18_154401) do
     t.integer "height"
     t.integer "weight"
     t.string "level"
-    t.boolean "availability"
+    t.string "availability"
     t.string "first_name"
     t.string "last_name"
+    t.string "conditions"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
