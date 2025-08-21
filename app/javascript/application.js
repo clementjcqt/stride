59183e983949f41fbbd765a2998b86f2e05db770
Annouchka Vandeville <annouchka.vandeville@kedgebs.com>
2025-08-21 13:10:23 +0200

    controller+show

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,10 @@
 import { Application } from "@hotwired/stimulus"
-import { FlipController } from "./controllers/flip_controller"
-
+import FlipController from "./controllers/flip_controller"
 const application = Application.start()
 application.register("flip", FlipController)
-
 // Optional: debug
 application.debug = true
 window.Stimulus = application
-
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Stride JS prêt")
 })
